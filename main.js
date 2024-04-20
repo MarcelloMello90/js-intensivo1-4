5e5db539807c61c0dfd8c22dece3d913ceac04db
marcelo <marcellomello90@gmail.com>
2024-04-20 14:33:19 +0200

    create project intensive 1-4

---
+++ main.js
@@ -0,0 +1,122 @@
+
+// Exercício 01
+  alert("Hello World!");
+
+
+
+
+//Exercicio 02
+  // Declaração de duas variáveis
+  var numero1 = 10;
+  var numero2 = 20;
+
+
+
+  // Soma das duas variáveis
+  var resultado = numero1 + numero2;
+
+  // Exibição do resultado
+  console.log("O resultado da soma é: " + resultado);
+
+
+
+//Exercicio 03
+
+  // Declaração de uma variável
+  var valor = "123" ;
+
+  // Verificação se o valor é um número usando o operador typeof
+  if (typeof valor === 'number' && !isNaN(valor)) {
+      console.log("É um número");
+  } else {
+      console.log("Não é um número");
+  }
+
+
+
+//Exercício 04
+
+  // Declaração de uma variável
+  var valor = "Olá, mundo!";
+
+  // Verificação se o valor é uma string usando o operador typeof
+  if (typeof valor === 'string') {
+      console.log("É uma string");
+  } else {
+      console.log("Não é uma string");
+  }
+
+
+//Exercício 05
+  // Declaração de uma variável
+  var valor = true;
+
+  // Verificação se o valor é um booleano usando o operador typeof
+  if (typeof valor === 'boolean') {
+      console.log("É um booleano");
+  } else {
+      console.log("Não é um booleano");
+  }
+
+
+//Exercício 06
+  // Declaração de duas variáveis
+  var numero1 = 20;
+  var numero2 = 10;
+
+  // Subtração das duas variáveis
+  var resultado = numero1 - numero2;
+
+  // Exibição do resultado
+  console.log("O resultado da subtração é: " + resultado);
+
+
+//Exercicio 07
+  // Declaração de duas variáveis
+  var numero1 = 5;
+  var numero2 = 8;
+
+  // Multiplicação das duas variáveis
+  var resultado = numero1 * numero2;
+
+  // Exibição do resultado
+  console.log("O resultado da multiplicação é: " + resultado);
+
+   
+
+//Exercicio 08
+
+  // Declaração de duas variáveis
+  var numero1 = 20;
+  var numero2 = 5;
+
+  // Divisão das duas variáveis
+  var resultado = numero1 / numero2;
+
+  // Exibição do resultado
+  console.log("O resultado da divisão é: " + resultado);
+
+
+
+//Exercicio 09
+
+  // Declaração de uma variável
+  var numero = 10;
+
+  // Verificação se o valor é um número par
+  if (numero % 2 === 0) {
+      console.log("É um número par");
+  } else {
+      console.log("Não é um número par");
+  }
+
+//Exercicio 10
+  // Declaração de uma variável
+  var numero = 11;
+
+  // Verificação se o valor é um número ímpar
+  if (numero % 2 !== 0) {
+      console.log("É um número ímpar");
+  } else {
+      console.log("Não é um número ímpar");
+  }
